83ed3693a846838cdfd003811ee5e9c6b950abd5
Edgar Montes <montesje@detroitmi.gov>
2018-07-09 18:30:57 -0400

    adding location and polling markers

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -36,21 +36,38 @@ class Map extends Component {
     document.getElementById('geocoder').appendChild(this.geocoder.onAdd(this.map));
 
     this.map.on("load", () => {
-      this.map.addSource('single-point', {
+      this.map.addSource('home', {
         "type": "geojson",
         "data": {
             "typdata.features[i].propertiese": "FeatureCollection",
             "features": []
         }
-    });
+      });
 
       this.map.addLayer({
-          "id": "point",
-          "source": "single-point",
+        "id": "home",
+        "source": "home",
+        "type": "circle",
+        "paint": {
+            "circle-radius": 10,
+            "circle-color": "#007cbf"
+        }
+      });
+      this.map.addSource('poll', {
+        "type": "geojson",
+        "data": {
+            "typdata.features[i].propertiese": "FeatureCollection",
+            "features": []
+        }
+      });
+
+      this.map.addLayer({
+          "id": "poll",
+          "source": "poll",
           "type": "circle",
           "paint": {
               "circle-radius": 10,
-              "circle-color": "red"
+              "circle-color": "#CF3234"
           }
       });
     });
@@ -59,7 +76,7 @@ class Map extends Component {
   getUserResults(e, map){
     console.log(e);
     console.log(map);
-    
+    this.map.getSource('home').setData(e.result.geometry);
     let url = `https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/Election_Boundaries_2018/FeatureServer/0//query?where=&objectIds=&time=&geometry=${e.result.center[0]}%2C${e.result.center[1]}+&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelWithin&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=geojson&token=`;
     fetch(url)
     .then((resp) => resp.json()) // Transform the data into json
@@ -67,7 +84,7 @@ class Map extends Component {
       console.log(data);
       let pollingPlaceId = null;
       for (let i = 0; i < data.features.length; i++) {
-        if(data.features[i].properties.boundary_t == 'Election Precincts'){
+        if(data.features[i].properties.boundary_t === 'Election Precincts'){
           pollingPlaceId = i;
           break;
         }
@@ -79,12 +96,9 @@ class Map extends Component {
         point.push(parseFloat(element));
       });
       point = turf.point(point);
+  
+      map.getSource('poll').setData(point.geometry);
       
-      map.getSource('single-point').setData(point.geometry);
-      map.flyTo({
-        center: data.features[pollingPlaceId].properties.pollxy.split(','),
-        zoom: 14
-      });
       document.querySelector('.info').innerHTML = `
       <div class="sign-up">
         <span>SIGN UP</span>
@@ -98,10 +112,10 @@ class Map extends Component {
       ${data.features.map(feature => `
         <div class="item">
           <span>${feature.properties.boundary_t}</span>
-          ${feature.properties.representa != '' ? 
-          `<strong>LOCATION:</strong> ${data.features[7].properties.precinct_n}<br>
-          <strong>ADDRESS:</strong> ${data.features[7].properties.precinct_l}<br>` : `<strong>LOCATION:</strong> ${data.features[7].properties.precinct_n}<br>
-          <strong>ADDRESS:</strong> ${data.features[7].properties.precinct_l}<br>`}
+          ${feature.properties.representa !== ' ' ? 
+          `<strong>DISTRICT:</strong> ${feature.properties.district_l}<br>
+          <strong>ELECTED OFFICIAL:</strong> ${feature.properties.representa}<br>` : `<strong>LOCATION:</strong> ${feature.properties.precinct_n}<br>
+          <strong>ADDRESS:</strong> ${feature.properties.precinct_l}<br>`}
         </div>
         `
       ).join('')}
--- src/components/panel/Panel.js
+++ src/components/panel/Panel.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './Panel.css';
 import logo from './logo.svg';
-const MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
 
 class Panel extends Component {
   render() {
