6145c07b6549768d860497ef57184c19a7c76ef5
Edgar Montes <jedgar1mx@gmail.com>
2019-12-11 12:05:32 -0500

    Updates

---
+++ precache-manifest.69d0118003f1de3e13c4ddddb20d85f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a503dc4a1b3a9042d0654f552b82eaf1",
+    "url": "/elections/index.html"
+  },
+  {
+    "revision": "1adeaa16367b1814406b",
+    "url": "/elections/static/css/2.37efe7f3.chunk.css"
+  },
+  {
+    "revision": "b66e2ebf95c824c4986c",
+    "url": "/elections/static/css/main.6873162a.chunk.css"
+  },
+  {
+    "revision": "1adeaa16367b1814406b",
+    "url": "/elections/static/js/2.6de3cb48.chunk.js"
+  },
+  {
+    "revision": "756105bd3a338b8faa72df666ea38d98",
+    "url": "/elections/static/js/2.6de3cb48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b66e2ebf95c824c4986c",
+    "url": "/elections/static/js/main.eae1c44c.chunk.js"
+  },
+  {
+    "revision": "0b8247ba7bb547532337",
+    "url": "/elections/static/js/runtime-main.c72052c9.js"
+  },
+  {
+    "revision": "3c15a0bdecbe50211480e8b9b7a32701",
+    "url": "/elections/static/media/logo.3c15a0bd.png"
+  }
+]);
