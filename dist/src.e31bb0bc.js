42237e933b84f64a04b0d2036c5c867e703de591
Edgar Montes <jedgar1mx@gmail.com>
2020-07-02 16:34:19 -0400

    Fixing geocoder text

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -54825,7 +54825,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56855" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58371" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
